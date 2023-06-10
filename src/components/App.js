7c19a49b84406c84f9580516526714b4c7044a94
SoloviovO <soloviov.o.m@gmail.com>
2023-06-10 21:24:28 +0300

    feature feedback modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import ModalFeedback from "./ModalFeedback/ModalFeedback";
+import FormFeedback from "./FormFeedback/FormFeedback";
+
 function App() {
-  return <div className="App"></div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalClose = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <div className="App"></div>
+      <button onClick={handleModalClose}>Open</button>
+      {isOpen && (
+        <ModalFeedback onClose={handleModalClose}>
+          <FormFeedback />
+        </ModalFeedback>
+      )}
+    </>
+  );
 }
 
 export default App;
