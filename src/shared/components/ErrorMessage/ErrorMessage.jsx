24adc7e8cd460755f91602f5bd97ce4a7a23fe97
Rdk001 <rmaslenchenko95@gmail.com>
2023-06-15 22:40:40 +0200

    Updates

--- src/shared/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ErrorMessage } from "formik";
-
-const ErrorMessageField = ({ name, className }) => {
-  return <ErrorMessage name={name} component="div" className={className} />;
-};
-
-export default ErrorMessageField;
