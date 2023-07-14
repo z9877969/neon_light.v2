6b8c2f290a9534a24aae1797c516f2809f395581
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-07-14 14:38:10 +0300

    add new bg

--- src/images/index.js
+++ src/images/index.js
@@ -1,15 +1,18 @@
-import background1 from "./bg1@1x.jpg";
-import background1x2 from "./bg1@2x.jpg";
-import background2 from "./bg2@1x.jpg";
-import background2x2 from "./bg2@2x.jpg";
-import background3 from "./bg3@1x.jpg";
-import background3x2 from "./bg3@2x.jpg";
-import background4 from "./bg4@1x.jpg";
-import background4x2 from "./bg4@2x.jpg";
+import background1 from "./bg01@1x-min.jpg";
+import background1x2 from "./bg01@2x-min.jpg";
+import background2 from "./bg02@1x-min.jpg";
+import background2x2 from "./bg02@2x-min.jpg";
+import background3 from "./bg03@1x-min.jpg";
+import background3x2 from "./bg03@2x-min.jpg";
+import background4 from "./bg04@1x-min.jpg";
+import background4x2 from "./bg04@2x-min.jpg";
+import background5 from "./bg05@1x-min.jpg";
+import background5x2 from "./bg05@2x-min.jpg";
 
 export const backgrounds = [
   { value: "background1", imageUrl: background1, imageUrl2x: background1x2 },
   { value: "background2", imageUrl: background2, imageUrl2x: background2x2 },
   { value: "background3", imageUrl: background3, imageUrl2x: background3x2 },
   { value: "background4", imageUrl: background4, imageUrl2x: background4x2 },
+  { value: "background5", imageUrl: background5, imageUrl2x: background5x2 },
 ];
