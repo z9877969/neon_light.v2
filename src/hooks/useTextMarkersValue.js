a83c72ed7bfd55a869bea72de79063ad031fc5ae
z9877969 <z9877969@ukr.net>
2023-07-17 18:44:09 +0300

    adds correct calculation text sizes after input text and dynamic change width or height value

--- src/hooks/useTextMarkersValue.js
+++ src/hooks/useTextMarkersValue.js
@@ -6,8 +6,6 @@ const getIsSpaceInStrEnd = (str) => {
   return str.split("\n").some((el) => el.slice(-1) === " ");
 };
 
-let render = 0;
-
 export const useTextMarkersValue = ({
   widthMarker,
   heightMarker,
@@ -15,168 +13,74 @@ export const useTextMarkersValue = ({
   setTextHeight,
   text,
 }) => {
-  const [xK, setXK] = useState(1);
-  const [yK, setYK] = useState(1);
+  // const [xK, setXK] = useState(1);
+  const [k, setK] = useState(1);
 
   const textRef = useRef(null);
-  // const prevNodeWidthRef = useRef(0);
-  // const prevNodeHeightRef = useRef(0);
-  // const prevWidthMarkerRef = useRef(0);
-  // const prevHeighMarkerRef = useRef(0);
 
   const firstRenderRef = useRef(true);
+  const prevKRef = useRef(1);
 
   const calcK = useCallback((markerValue, nodeSize) => {
     return markerValue / nodeSize;
   }, []);
   const calcMarkerValue = useCallback((k, nodeSize) => {
-    return k * nodeSize;
+    return Math.round(k * nodeSize);
   }, []);
 
+  // + calc k | set base ratio beetween textHeight and nodeYSize and set base textWidth by textHeight
   useEffect(() => {
-    render += 1;
-  });
-
-  // calc xK | ratio beetween text container x size and x marker value
-  useEffect(() => {
-    if (firstRenderRef.current) return;
+    if (!firstRenderRef.current) return;
     const { width: nodeXSize, height: nodeYSize } = getNodeSizes(
       textRef.current
     );
-    const xK = calcK(widthMarker, nodeXSize);
-
-    if (xK === yK) return;
-    setXK(xK);
-  }, [calcK, yK, widthMarker]);
-  // calc xK -END
+    const k = calcK(heightMarker, nodeYSize);
+    // if (k === xK) return;
+    setK(k);
+    setTextWidth(calcMarkerValue(k, nodeXSize));
+    prevKRef.current = k;
+  }, [heightMarker, calcK, calcMarkerValue, setTextWidth]);
+  // calc k | set base ratio beetween textHeight and nodeYSize and set base textWidth by textHeight -END
   // ===
-  // calc yK | ratio beetween text container y size and y marker value
-  useEffect(() => {
-    const { width: nodeXSize, height: nodeYSize } = getNodeSizes(
-      textRef.current
-    );
-    const yK = calcK(heightMarker, nodeYSize);
-
-    setYK(yK);
-    // setTextWidth(calcMarkerValue(yK, nodeXSize));
-  }, [
-    heightMarker,
-    calcK,
-    //  calcMarkerValue, setTextWidth
-  ]);
-  // calc xK -END
-
-  // change widthMarker by changed yK
+  // set textHeight by textWidth
   useEffect(() => {
     if (firstRenderRef.current) return;
     const { width: nodeXSize, height: nodeYSize } = getNodeSizes(
       textRef.current
     );
-    setTextWidth(nodeXSize * yK);
-  }, [yK, setTextWidth]);
-  // change widthMarker by changed yK -END
+    const k = calcK(widthMarker, nodeXSize);
+    if (k === prevKRef.current) return;
+    setK(k);
+    prevKRef.current = k;
+    setTextHeight(calcMarkerValue(k, nodeYSize));
+  }, [widthMarker, calcK, calcMarkerValue, setTextHeight]);
+  // set textHeight by textWidth -END
 
-  // // change widthMarker after changing heightMarker
-  // /*
-  //   new node width
-  //     -> change width value
-  //   new height value && not new node width
-  //     -> change width value
-  // */
-  // useEffect(() => {
-  //   const heightMarkerNum = Number(heightMarker); // 1r -> 8 | 2r -> 8
-  //   if (!heightMarkerNum || getIsSpaceInStrEnd(text)) return; // 1r -> continue | 2r -> continue
+  // set textWidth by textHeight
+  useEffect(() => {
+    if (firstRenderRef.current) return;
+    const { width: nodeXSize, height: nodeYSize } = getNodeSizes(
+      textRef.current
+    );
+    const k = calcK(heightMarker, nodeYSize);
+    if (k === prevKRef.current) return;
+    const newMarkerWidth = calcMarkerValue(k, nodeXSize);
+    setTextWidth(newMarkerWidth);
+  }, [heightMarker, calcK, calcMarkerValue, setTextWidth]);
+  // set textWidth by textHeight -END
 
-  //   const { width: nodeWidth, height: nodeHeight } = getNodeSizes(
-  //     textRef.current
-  //   ); // 1r -> nW: 27, nH 58 | 2r -> nW: 27, nH 56
-
-  //   console.log("text.length :>> ", text.length);
-
-  //   const prevHeightMarker = prevHeighMarkerRef.current; // 1r -> 0 | 2r -> 8
-  //   const prevNodeWidth = prevNodeWidthRef.current; // 1r -> 27
-  //   const isChangedHeightMarker = prevHeightMarker !== heightMarkerNum; // 1r -> true | 2r -> false
-  //   const isChangedNodeWidth = prevNodeWidth !== nodeWidth; // 1r -> true | 2r -> false
-
-  //   // console.group("change width marker");
-  //   // console.log("render :>> ", render);
-  //   // console.log("nodeWidth", nodeWidth);
-  //   // console.log("nodeHeight", nodeHeight);
-  //   // console.log("prevHeightMarker", prevHeightMarker);
-  //   // console.log("prevNodeWidth", prevNodeWidth);
-  //   // console.log("isChangedHeightMarker", isChangedHeightMarker);
-  //   // console.log("isChangedNodeWidth", isChangedNodeWidth);
-  //   // console.log(
-  //   //   "canChangeWidthMarker :>> ",
-  //   //   isChangedNodeWidth || (isChangedHeightMarker && !isChangedNodeWidth)
-  //   // );
-  //   // console.groupEnd();
-
-  //   if (isChangedNodeWidth || (isChangedHeightMarker && !isChangedNodeWidth)) {
-  //     // 1r -> continue| 2r -> return
-  //     prevNodeWidthRef.current = nodeWidth; // 1r -> 27
-  //     prevHeighMarkerRef.current = heightMarkerNum; // 1r -> 8
-
-  //     const heightRatio = heightMarkerNum / nodeHeight; // 1r -> 8/56 // k = cm/px -> markerValue = cm/px * px -> k * nodeWidth
-  //     setTextWidth(Math.round(nodeWidth * heightRatio)); // 1r -> 27*8/56 = 4
-  //   }
-  // }, [
-  //   heightMarker, // change true
-  //   widthMarker, // change true
-  //   text, // change true
-  //   setTextWidth, // change false
-  //   prevNodeWidthRef, // change false
-  // ]);
-  // // change widthMarker after changing heightMarker -END
-  // // ===
-  // // change heightMarker after changing widthMarker
-  // /*
-  //   new node height
-  //     -> change height value
-  //   new width value && not new node height
-  //     -> change height value
-  // */
-  // useEffect(() => {
-  //   const widthMarkerNum = Number(widthMarker); // 1r -> 0 | 2r ->
-  //   if (!widthMarkerNum || getIsSpaceInStrEnd(text)) return; // 1r -> return
-
-  //   const { width: nodeWidth, height: nodeHeight } = getNodeSizes(
-  //     textRef.current
-  //   );
-
-  //   const prevNodeHeight = prevNodeHeightRef.current;
-  //   const prevWidthMarker = prevWidthMarkerRef.current;
-  //   const isChangedWidthMarkerValue = prevWidthMarker !== widthMarkerNum;
-  //   const isChangedNodeHeight = nodeHeight !== prevNodeHeight;
-  //   // console.group("change height marker");
-  //   // console.log("render :>> ", render);
-  //   // console.log("nodeWidth", nodeWidth);
-  //   // console.log("nodeHeight", nodeHeight);
-  //   // console.log("prevNodeHeight", prevNodeHeight);
-  //   // console.log("prevWidthMarker", prevWidthMarker);
-  //   // console.log("isChangedWidthMarkerValue", isChangedWidthMarkerValue);
-  //   // console.log("isChangedNodeHeight", isChangedNodeHeight);
-  //   // console.log(
-  //   //   "canChangeHeightMarker :>>",
-  //   //   isChangedNodeHeight || (isChangedWidthMarkerValue && !isChangedNodeHeight)
-  //   // );
-  //   // console.groupEnd();
-  //   if (
-  //     isChangedNodeHeight ||
-  //     (isChangedWidthMarkerValue && !isChangedNodeHeight)
-  //   ) {
-  //     prevNodeHeightRef.current = nodeHeight;
-  //     prevWidthMarkerRef.current = widthMarkerNum;
-  //     const widthRatio = widthMarkerNum / nodeWidth;
-  //     setTextHeight(Math.round(nodeHeight * widthRatio));
-  //   }
-  // }, [
-  //   widthMarker, // change true
-  //   text, // change true
-  //   setTextHeight, // change false
-  //   prevNodeHeightRef, // change false
-  // ]);
-  // // change heightMarker after changing widthMarker - END
+  // change markers by changed text
+  useEffect(() => {
+    if (firstRenderRef.current) return;
+    const { width: nodeXSize, height: nodeYSize } = getNodeSizes(
+      textRef.current
+    );
+    const newWidthMarker = calcMarkerValue(k, nodeXSize);
+    setTextWidth(newWidthMarker);
+    const newHeightMarker = calcMarkerValue(k, nodeYSize);
+    setTextHeight(newHeightMarker);
+  }, [text, k, calcMarkerValue, setTextWidth, setTextHeight]);
+  // change markers by changed text -END
 
   useEffect(() => {
     firstRenderRef.current = false;
