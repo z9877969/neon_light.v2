ab265df3d25a65188138a96dd2086b69e4f960b2
z9877969 <z9877969@ukr.net>
2023-07-31 14:47:48 +0300

    fixes calc price by font size

--- src/hooks/usePrice.js
+++ src/hooks/usePrice.js
@@ -3,28 +3,38 @@ import { useEffect, useState } from "react";
 import calculatePrice from "../shared/lib/priceCalculator";
 import getNeonStripLength from "../shared/lib/getNeonStripLength";
 
+const changeNumByComma = (num, numsAfterCommaAmount = 0) => {
+  const devider = Math.pow(10, numsAfterCommaAmount);
+  return Math.round(num * devider) / devider;
+};
+
 const usePrice = ({ text, textWidth, textHeight }) => {
   const [price, setPrice] = useState(0);
 
   useEffect(() => {
     if (text && textWidth && textHeight) {
-      const symbolQuantityText = text.split(" ").join("").length;
+      const textSymbolQuantity = text
+        .split(" ")
+        .join("")
+        .split("\n")
+        .join("").length;
+      const textRows = text.split("\n").length;
+      const textFontSize = textHeight / textRows;
+
       const lengthOfLedStripInMeters = getNeonStripLength(
-        symbolQuantityText,
-        Number(textHeight)
+        textSymbolQuantity,
+        textFontSize
       );
       const totalPrice = calculatePrice(
-        textWidth / 100,
-        textHeight / 100,
-        symbolQuantityText,
+        changeNumByComma(textWidth) / 100,
+        changeNumByComma(textHeight) / 100,
+        textSymbolQuantity,
         lengthOfLedStripInMeters / 100
       );
-      console.log("totalPrice :>> ", totalPrice);
-      console.log("textWidth / 100", textWidth / 100);
-      console.log("textHeight / 100", textHeight / 100);
-      console.log("symbolQuantityText", symbolQuantityText);
-      console.log("lengthOfLedStripInMeters", lengthOfLedStripInMeters / 100);
       setPrice(Math.round(totalPrice));
+    }
+    if (!text) {
+      setPrice(0);
     }
   }, [text, textHeight, textWidth]);
 
